d72b9532d24ec0c14f7c3e9c2b78684ee60a45da
glaukiol1 <glaukiol1@gmail.com>
2022-01-21 22:48:38 +0100

    small changes

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math/big"
+	"os"
+	"strings"
 
 	"github.com/glaukiol1/Ethereum-Wallet-Stealer/src"
 )
@@ -17,17 +19,36 @@ func main() {
 		_pk := src.PrivateKeyToHex(pk)
 		addresses = append(addresses, _pb)
 		privateKeys = append(privateKeys, _pk)
-		if i == 1000 {
-			i = 0
-			for j := 0; j < len(addresses); j++ {
-				adr := addresses[j]
-				pkr := privateKeys[j]
-				if src.HasBalance(adr).Cmp(big.NewFloat(0)) == 1 {
-					fmt.Println("HIT! Address: " + adr + " Seed: " + pkr)
-				} else {
-					fmt.Println("Missed!")
-				}
-			}
+		if i == 1000000 {
+			break
+		}
+	}
+	eachFile := len(addresses) / 3
+	pb1, _ := os.Create("./public1.out")
+	pk1, _ := os.Create("./private1.out")
+
+	pb1.WriteString(strings.Join(addresses[0:eachFile], "\n"))
+	pk1.WriteString(strings.Join(privateKeys[0:eachFile], "\n"))
+
+	pb2, _ := os.Create("./public2.out")
+	pk2, _ := os.Create("./private2.out")
+
+	pb2.WriteString(strings.Join(addresses[0:eachFile], "\n"))
+	pk2.WriteString(strings.Join(privateKeys[0:eachFile], "\n"))
+
+	pb3, _ := os.Create("./public3.out")
+	pk3, _ := os.Create("./private3.out")
+
+	pb3.WriteString(strings.Join(addresses[0:eachFile], "\n"))
+	pk3.WriteString(strings.Join(privateKeys[0:eachFile], "\n"))
+
+	for j := 0; j < len(addresses); j++ {
+		adr := addresses[j]
+		pkr := privateKeys[j]
+		if src.HasBalance(adr).Cmp(big.NewFloat(0)) == 1 {
+			fmt.Println("HIT! Address: " + adr + " Seed: " + pkr)
+		} else {
+			fmt.Println("Missed! " + adr)
 		}
 	}
 }
