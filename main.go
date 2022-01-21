ada26f8de0ac631e64017165c34397e05c89c238
glaukiol1 <glaukiol1@gmail.com>
2022-01-21 23:26:42 +0100

    finishing touches

--- main.go
+++ main.go
@@ -1,11 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"math/big"
-	"os"
-	"strings"
-
 	"github.com/glaukiol1/Ethereum-Wallet-Stealer/src"
 )
 
@@ -13,42 +8,16 @@ var addresses []string
 var privateKeys []string
 
 func main() {
+	hashes := 500000 // five hundred thousand 500,000
 	for i := 0; true; i++ {
 		pb, pk := src.Keygen()
 		_pb := src.PubkeyToAddress(pb)
 		_pk := src.PrivateKeyToHex(pk)
 		addresses = append(addresses, _pb)
 		privateKeys = append(privateKeys, _pk)
-		if i == 1000000 {
+		if i == hashes {
 			break
 		}
 	}
-	eachFile := len(addresses) / 3
-	pb1, _ := os.Create("./public1.out")
-	pk1, _ := os.Create("./private1.out")
-
-	pb1.WriteString(strings.Join(addresses[0:eachFile], "\n"))
-	pk1.WriteString(strings.Join(privateKeys[0:eachFile], "\n"))
-
-	pb2, _ := os.Create("./public2.out")
-	pk2, _ := os.Create("./private2.out")
-
-	pb2.WriteString(strings.Join(addresses[0:eachFile], "\n"))
-	pk2.WriteString(strings.Join(privateKeys[0:eachFile], "\n"))
-
-	pb3, _ := os.Create("./public3.out")
-	pk3, _ := os.Create("./private3.out")
-
-	pb3.WriteString(strings.Join(addresses[0:eachFile], "\n"))
-	pk3.WriteString(strings.Join(privateKeys[0:eachFile], "\n"))
-
-	for j := 0; j < len(addresses); j++ {
-		adr := addresses[j]
-		pkr := privateKeys[j]
-		if src.HasBalance(adr).Cmp(big.NewFloat(0)) == 1 {
-			fmt.Println("HIT! Address: " + adr + " Seed: " + pkr)
-		} else {
-			fmt.Println("Missed! " + adr)
-		}
-	}
+	src.BalanceList(addresses, privateKeys)
 }
--- src/hasBalance.go
+++ src/hasBalance.go
@@ -2,11 +2,14 @@ package src
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"math/big"
 	"net/http"
+	"os"
 	"strconv"
+	"strings"
 
 	"github.com/ethereum/go-ethereum/params"
 )
@@ -47,4 +50,70 @@ func HasBalance(address string) *big.Float {
 	}
 	intn, _ := strconv.Atoi(res.Result)
 	return weiToEther(big.NewInt(int64(intn)))
+}
+
+type s1 struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+	Result  []struct {
+		Account string `json:"account"`
+		Balance string `json:"balance"`
+	} `json:"result"`
+}
+
+func chunkSlice(slice []string, chunkSize int) [][]string {
+	var chunks [][]string
+	for i := 0; i < len(slice); i += chunkSize {
+		end := i + chunkSize
+
+		// necessary check to avoid slicing beyond
+		// slice capacity
+		if end > len(slice) {
+			end = len(slice)
+		}
+
+		chunks = append(chunks, slice[i:end])
+	}
+
+	return chunks
+}
+
+func BalanceList(addressList []string, pkList []string) {
+	list := chunkSlice(addressList, 20)
+	for _, lst := range list {
+		resp, err := http.Get(
+			"https://api.etherscan.io/api?module=account&action=balancemulti&address=" + strings.Join(lst, ",") + "&tag=latest&apikey=HVIFHDEQKUUCMPB7FB6CHGBC7CPG6ZIWX6")
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var res s1
+		if resp.StatusCode != 200 {
+			panic("Non-200 Status Code")
+		}
+		err = json.Unmarshal(body, &res)
+		if err != nil {
+			if strings.Index(string(body), "Max rate limit reached") != -1 {
+				println("Rate limited")
+				continue
+			} else {
+				panic(err)
+			}
+		}
+		for i := 0; i < len(res.Result); i++ {
+			current := res.Result[i]
+			n, _ := strconv.Atoi(current.Balance)
+			bal := weiToEther(big.NewInt(int64(n)))
+			if bal.Cmp(big.NewFloat(0)) == 1 {
+				fmt.Println("Hit! \n\r Public Key: " + current.Account + "\n\r Private Key: " + pkList[i] + "\n\r Find this data in SUCCESS.txt")
+				f, _ := os.Create("./SUCCESS.txt")
+				f.WriteString("Address: " + current.Account + "\nPrivate Key: " + pkList[i])
+			}
+		}
+	}
 }
